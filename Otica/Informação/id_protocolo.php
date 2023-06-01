f4589df7a635693903ee194721fc7b066dcbe1a9
Danphas <107813437+Danphas@users.noreply.github.com>
2023-06-01 10:04:55 -0300

    Update id_protocolo.php

--- Otica/Informação/id_protocolo.php
+++ Otica/Informação/id_protocolo.php
@@ -63,14 +63,14 @@ if (isset($_GET['id'])) {
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+if (isset($_POST['return'])) {
     $editedOD = $_POST['od'];
-    $editedOE = $_POST['oe'];
+    $editedOE = $_POST['ed'];
     $editedMedico = $_POST['medico'];
     $editedValor = $_POST['valor'];
     $editedObservacoes = $_POST['obs'];
 
-    $sql_update = "UPDATE vendas SET od = '$editedOD', oe = '$editedOE', medico = '$editedMedico', valor = '$editedValor', observacoes = '$editedObservacoes' WHERE id_compra = '$id_compra'";
+    $sql_update = "UPDATE vendas SET od = '$editedOD', oe = '$editedOE', medico = '$editedMedico', valor = '$editedValor', obs = '$editedObservacoes' WHERE id_compra = '$id_compra'";
     if ($mysqli->query($sql_update)) {
         header("Location: /Cliente/cliente.php?id=$cliente_id");
         exit();
@@ -88,7 +88,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>Danpha's</title>
+    <title>Informação</title>
 </head>
 
 <body>
@@ -115,38 +115,45 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <div class="box-client">
             <div id="direita">
                 <span style="float: right; margin: 5px;">
-                    Usuario Responsavel :
+                    <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Usuario Responsavel
+                        :</span>
                     <span class="tbold">
                         <?php echo $usuario_responsavel; ?>
                     </span><br>
-                    Email :
+                    <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Email :</span>
                     <span class="tbold">
                         <?php echo $email; ?>
                     </span><br>
-                    Celular :
+                    <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Celular :</span>
                     <span class="tbold">
                         <?php echo $celular; ?>
                     </span><br>
-                    Telefone :
+                    <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Telefone :</span>
                     <span class="tbold">
                         <?php echo $telefone; ?>
                     </span><br>
                 </span>
             </div>
             <div id="esquerda" style="margin: 5px;">
-                Cliente ID :
+                <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Cliente ID :</span>
                 <span class="tbold">
                     <?php echo $cliente_id; ?>
                 </span><br>
-                Nome Cliente :
+                <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Nome Cliente :</span>
                 <span class="tbold">
                     <?php echo $nome_cliente; ?>
                 </span><br>
-                CPF/CNPJ :
+                <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">CPF/CNPJ :</span>
                 <span class="tbold">
-                    <?php echo $cpf; ?>
+                    <?php
+                    $cpf;
+                    $formatted_cpf = substr_replace($cpf, '.', 3, 0);
+                    $formatted_cpf = substr_replace($formatted_cpf, '.', 7, 0);
+                    $formatted_cpf = substr_replace($formatted_cpf, '-', 11, 0);
+                    echo $formatted_cpf;
+                    ?>
                 </span><br>
-                Cidade :
+                <span id="user-inf" style="font-family: Arial, sans-serif; font-weight: bold;">Cidade :</span>
                 <span class="tbold">
                     <?php echo $cidade; ?>
                 </span><br>
@@ -158,31 +165,30 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <section class="header">
 
         <div class="form-container">
-            <h2>ID Compra</h2>
             <form method="POST" action="\Cliente\cliente.php?id=<?php echo $cliente_id; ?>">
                 <div class="returnb">
                     <button type="submit" name="return">Retornar</button>
                 </div>
                 <div class="row">
                     <div class="left">
-                        <label for="od">OD</label>
+                        <label for="od" style="font-weight: bold">OD</label>
                         <input type="text" id="od" name="od" value="<?php echo $od; ?>" required>
 
-                        <label for="medico">Médico</label>
+                        <label for="medico" style="font-weight: bold">Médico</label>
                         <input type="text" id="medico" name="medico" value="<?php echo $medico; ?>" required>
                     </div>
 
                     <div class="right">
-                        <label for="ed">OE</label>
+                        <label for="ed" style="font-weight: bold">OE</label>
                         <input type="text" id="ed" name="ed" value="<?php echo $oe; ?>" required>
 
-                        <label for="valor">Valor</label>
+                        <label for="valor" style="font-weight: bold">Valor</label>
                         <input type="number" id="valor" name="valor" step="0.01" class="valor"
                             value="<?php echo $valor; ?>" required>
                     </div>
                 </div>
 
-                <label for="obs">Observações</label>
+                <label for="obs" style="font-weight: bold">Observações</label>
                 <textarea id="obs" name="obs"><?php echo $obs; ?></textarea>
             </form>
         </div>
