62946af1950f2a6560cec018d1082fc88dfb7e4e
itjuanpablo <jp664165@gmail.com>
2023-10-13 21:55:28 -0300

    MOD: bat e scripts db inseridos

--- Otica.bat
+++
@@ -1,4 +0,0 @@
-start "" "C:\wamp64\wampmanager.exe"
-timeout /t 55 /nobreak
-start "" "\Otica\Otica\update-files\bin\update-files.exe"
-start chrome http://www.otica/Login/login.php
---
+++ Otica/Otica.bat
@@ -0,0 +1,4 @@
+start "" "C:\wamp64\wampmanager.exe"
+timeout /t 55 /nobreak
+start "" "\Otica\Otica\update-files\bin\update-files.exe"
+start chrome http://www.otica/Login/login.php
