eed1ef64d8f57c7ad15fc610e5d362fcacf79e31
itjuanpablo <jp664165@gmail.com>
2023-10-15 15:08:31 -0300

    MOD: roolback

--- Otica/Cadastro/cadastro.php
+++ Otica/Cadastro/cadastro.php
@@ -162,7 +162,7 @@ function obterUltimoIDGeral($mysqli)
       </div>
 
       <div class="button">
-        <input type="submit" value="Cadastrar teste">
+        <input type="submit" value="Cadastrar">
       </div>
 
       <div class="button">
