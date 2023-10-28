6602b936c9e9807849682a823da06373cdf08af1
itjuanpablo <jp664165@gmail.com>
2023-10-28 14:49:28 -0300

    MOD inserção --`default-character-set=utf8mb4`

--- Otica/scripts/Exec-Bkp_DB.ps1
+++ Otica/scripts/Exec-Bkp_DB.ps1
@@ -1,7 +1,11 @@
+$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
+$backupName = "backup"
+
+# Use o caminho correto para o mysqldump.exe
 $mysqlDumpPath = "C:\wamp64\bin\mysql\mysql8.0.31\bin\mysqldump.exe"
 $databaseName = "db_otica"
 $outputPath = "C:\temp\$backupName_$timestamp.sql"
 
-# Comando para executar o mysqldump com codificação UTF-8
-$command = "$mysqlDumpPath -u root -p --default-character-set=utf8 $databaseName > $outputPath"
+# Comando para executar o mysqldump
+$command = "$mysqlDumpPath -u root -p --default-character-set=utf8mb4 $databaseName > $outputPath"
 Invoke-Expression $command
