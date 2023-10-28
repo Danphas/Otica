9f417e10d3aca6047071c3ac3e67fba5c7f60052
itjuanpablo <jp664165@gmail.com>
2023-10-28 11:10:56 -0300

    MOD charset utf-8 setado no bkp do banco de dados

--- Otica/scripts/Exec-Bkp_DB.ps1
+++ Otica/scripts/Exec-Bkp_DB.ps1
@@ -1,11 +1,7 @@
-$timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-$backupName = "backup"
-
-# Use o caminho correto para o mysqldump.exe
 $mysqlDumpPath = "C:\wamp64\bin\mysql\mysql8.0.31\bin\mysqldump.exe"
 $databaseName = "db_otica"
 $outputPath = "C:\temp\$backupName_$timestamp.sql"
 
-# Comando para executar o mysqldump
-$command = "$mysqlDumpPath -u root -p $databaseName > $outputPath"
+# Comando para executar o mysqldump com codificação UTF-8
+$command = "$mysqlDumpPath -u root -p --default-character-set=utf8 $databaseName > $outputPath"
 Invoke-Expression $command
